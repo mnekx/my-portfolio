67e7ed2151ac21b13e58d36c559f0deb8668cca5
Mnemba Chambuya <karimnembac@gmail.com>
2022-01-24 18:18:54 +0300

    update dynamics

--- js/dynamics.js
+++ js/dynamics.js
@@ -1,7 +1,14 @@
-let menu_icon=document.querySelector("#humberger_menu_icon");
-let menu_list=document.querySelector("#humberger_menu_list");
-menu_icon.addEventListener("click",function (){
-    console.log("Function");
-    menu_list.classList.toggle('show')
-}
-);
+const menuIcon = document.querySelector('#humberger_menu_icon');
+const menuList = document.querySelector('#humberger_menu_list');
+const closeMenuIcon = document.querySelector('#close-menu');
+menuIcon.addEventListener('click', () => {
+  closeMenuIcon.classList.add('show-icon');
+  menuList.classList.add('show');
+  menuIcon.classList.add('hide-icon');
+});
+
+closeMenuIcon.addEventListener('click', () => {
+  closeMenuIcon.classList.remove('show-icon');
+  menuList.classList.remove('show');
+  menuIcon.classList.remove('hide-icon');
+});
