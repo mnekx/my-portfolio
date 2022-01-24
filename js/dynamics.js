58dcdc15919edfc516992d866eb84b4488f00b90
souad988 <59707859+souad988@users.noreply.github.com>
2022-01-24 16:59:05 +0100

    Justify alignment for nav list

--- js/dynamics.js
+++ js/dynamics.js
@@ -1,14 +1,17 @@
 const menuIcon = document.querySelector('#humberger_menu_icon');
 const menuList = document.querySelector('#humberger_menu_list');
 const closeMenuIcon = document.querySelector('#close-menu');
+const logoHeader = document.querySelector(".logo h1");
 menuIcon.addEventListener('click', () => {
   closeMenuIcon.classList.add('show-icon');
   menuList.classList.add('show');
   menuIcon.classList.add('hide-icon');
+  logoHeader.classList.add('hide-icon');
 });
 
 closeMenuIcon.addEventListener('click', () => {
   closeMenuIcon.classList.remove('show-icon');
   menuList.classList.remove('show');
   menuIcon.classList.remove('hide-icon');
+  logoHeader.classList.remove('hide-icon');
 });
