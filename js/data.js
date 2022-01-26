8202056ad57dd8aaf94364de1510b5caedb6169c
Mnemba Chambuya <karimnembac@gmail.com>
2022-01-26 12:10:05 +0300

    add data.js

--- js/data.js
+++ js/data.js
@@ -1,89 +1,107 @@
 const projects = [
-    {
-        title: 'Multi-Post Strories',
-        screenshot: './images/modal-image.png',
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-        technologies: [
-            'css', 'html', 'bootstrap', 'ruby'
-        ]
-    },
-    {
-        title: 'Multi-Post Strories',
-        screenshot: './images/modal-image.png',
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-        technologies: [
-            'css', 'html', 'bootstrap', 'ruby'
-        ]
-    },
-    {
-        title: 'Multi-Post Strories',
-        screenshot: './images/modal-image.png',
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-        technologies: [
-            'css', 'html', 'bootstrap', 'ruby'
-        ]
-    },
-    {
-        title: 'Multi-Post Strories',
-        screenshot: './images/modal-image.png',
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-        technologies: [
-            'css', 'html', 'bootstrap', 'ruby'
-        ]
-    },
+  {
+    id: 'project1',
+    title: 'Multi-Post Strories',
+    screenshot: './images/modal-image.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    technologies: [
+      'css', 'html', 'bootstrap', 'ruby',
+    ],
+  },
+  {
+    id: 'project2',
+    title: 'Multi-Post Strories',
+    screenshot: './images/modal-image.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    technologies: [
+      'css', 'html', 'bootstrap', 'ruby',
+    ],
+  },
+  {
+    id: 'project3',
+    title: 'Multi-Post Strories',
+    screenshot: './images/modal-image.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    technologies: [
+      'css', 'html', 'bootstrap', 'ruby',
+    ],
+  },
+  {
+    id: 'project4',
+    title: 'Multi-Post Strories',
+    screenshot: './images/modal-image.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    technologies: [
+      'css', 'html', 'bootstrap', 'ruby',
+    ],
+  },
 
-]
+];
 
-let cardsContainer = document.querySelector('.cards')
+// Cards being loaded dynamically
+const cardsContainer = document.querySelector('.cards');
 
-for(let project of projects) {
-    let cardElement = document.createElement('div');
+for (let i = 0; i < projects.length; i += 1) {
+  const cardElement = document.createElement('div');
 
-cardsContainer.appendChild(cardElement)
+  cardsContainer.appendChild(cardElement);
 
-cardElement.className = 'card';
+  cardElement.className = 'card';
 
-let imgPlaceHolder = document.createElement('div');
+  const imgPlaceHolder = document.createElement('div');
 
-cardElement.appendChild(imgPlaceHolder); 
+  cardElement.appendChild(imgPlaceHolder);
 
-imgPlaceHolder.className = 'img-placeholder'
+  imgPlaceHolder.className = 'img-placeholder';
 
-let imgElement = document.createElement('img');
-imgElement.src = "./images/img-placeholder.png";
-imgElement.alt = "multi-post-stories-image";
+  const imgElement = document.createElement('img');
+  imgElement.src = './images/img-placeholder.png';
+  imgElement.alt = 'multi-post-stories-image';
 
-imgPlaceHolder.appendChild(imgElement);
+  imgPlaceHolder.appendChild(imgElement);
 
-let descSection = document.createElement('section');
-descSection.className = "card-desc"
+  const descSection = document.createElement('section');
+  descSection.className = 'card-desc';
 
-cardElement.appendChild(descSection);
+  cardElement.appendChild(descSection);
 
-let h3 = document.createElement('h3');
-h3.innerText = project['title']
+  const h3 = document.createElement('h3');
+  h3.innerText = projects[i].title;
 
-let p = document.createElement('p');
-p.innerText = project['description']
+  const p = document.createElement('p');
+  p.innerText = projects[i].description;
 
-descSection.appendChild(h3);
-descSection.appendChild(p)
+  descSection.appendChild(h3);
+  descSection.appendChild(p);
 
-let ul = document.createElement('ul');
+  const ul = document.createElement('ul');
 
-ul.className = 'lines-links';
-for(let link of project['technologies']) {
-    let li = document.createElement('li');
-    li.innerText = link
-    ul.appendChild(li)
+  ul.className = 'lines-links';
+  for (let link = 0; link < projects[i].technologies.length; link += 1) {
+    const li = document.createElement('li');
+    li.innerText = projects[i].technologies[link];
+    ul.appendChild(li);
+  }
+
+  cardElement.appendChild(ul);
+
+  const a = document.createElement('a');
+  a.classList.add('btn', 'btn-primary', 'open-modal');
+  a.innerText = 'See Project';
+  a.id = projects[i].id;
+
+  cardElement.appendChild(a);
 }
 
-cardElement.appendChild(ul)
+// The modal
+const modal = document.querySelector('.modal');
+const openModalBtns = document.querySelectorAll('.open-modal');
+const closeModalSpan = document.querySelector('#modal-closer');
 
-let a = document.createElement('a');
-a.classList.add('btn', 'btn-primary', 'open-modal');
-a.innerText = 'See Project'
+const projectbtnsArr = Array.from(openModalBtns);
 
-cardElement.appendChild(a);
-}
+// Paste the code here
 
+closeModalSpan.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
